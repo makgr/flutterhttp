bebbf28f4a6fef5e464cd8b940702ed41b4eb219
Mohammad Ali Khan <xvirus.bd@gmail.com>
2021-12-21 00:54:54 +0600

    cart screen updated

--- lib/screens/cartScreen.dart
+++ lib/screens/cartScreen.dart
@@ -14,10 +14,42 @@ class CartScreen extends StatelessWidget {
         future: ApiService().getCart(1),
         builder: (contex,AsyncSnapshot snapshot){
           if(snapshot.hasData){
-            return Text('success');
+            List products = snapshot.data['products'];
+            return ListView.builder(
+              itemCount: products.length,
+              itemBuilder: (contex,index){
+                return FutureBuilder(
+                  future: ApiService().getSingleProductDetails(products[index]['productId']),
+                  builder: (contex, AsyncSnapshot asyncSnapshot){
+                    if(asyncSnapshot.hasData){
+                      return ListTile(
+                        leading: Image.network(asyncSnapshot.data['image']),
+                        title: Text(asyncSnapshot.data['title']),
+                        subtitle: Text('Quantity: '+products[index]['quantity'].toString()),
+                        // trailing: Text('Price: '+asyncSnapshot.data['price'].toString()),
+                        trailing: IconButton(onPressed: (){}, icon: Icon(Icons.delete, color: Colors.red)),
+                      );
+                    }
+                    return LinearProgressIndicator();
+                  }
+                  );
+              }
+              );
           }
           return Center(child: CircularProgressIndicator(),);
         }
+        ),
+        bottomNavigationBar: Container(
+          height: 60,
+          width: double.infinity,
+          color: Colors.amber,
+          child: Center(
+            child: Text('Order Now', style: TextStyle(
+              color: Colors.black,
+              fontSize: 30,
+              
+            ),),
+          ),
         ),
       
     );
